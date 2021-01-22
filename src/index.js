58c5b1d4c3c67d892e7c3aaa26849500a3188fa4
Olga Uzun <67106495+ol4enok20101@users.noreply.github.com>
2021-01-22 16:35:33 +0200

    ready files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,35 @@
 import './styles.css';
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import refs from './js/refs';
+import fetchCountries from './js/fetch-countries';
+import aboutCountry from './templates/about-country.hbs';
+import countriesMarkup from './templates/country-list.hbs';
+import debounce from 'lodash.debounce';
+
+const errorMessage = () =>
+  error({
+    title: 'Warning!',
+    text: 'Too many matches found. Please enter a more specific query!',
+    delay: 2000,
+    closerHover: true,
+  });
+
+const countrySearch = debounce(() => {
+  refs.countriesRef.innerHTML = '';
+  fetchCountries(refs.input.value).then(response => {
+    if (response.length > 10) {
+      errorMessage();
+    } else if (response.length === 1) {
+      const countryMarkup = aboutCountry(response);
+      refs.aboutCountry.insertAdjacentHTML('beforeend', countryMarkup);
+      console.log(response);
+    } else {
+      const markup = countriesMarkup(response);
+      refs.countriesRef.insertAdjacentHTML('beforeend', markup);
+    }
+  });
+}, 1000);
+
+refs.input.addEventListener('input', countrySearch);
---
+++ src/js/refs.js
@@ -0,0 +1,7 @@
+const refs = {
+  countriesRef: document.querySelector('.countries'),
+  input: document.querySelector('.contriesInput'),
+  aboutCountry: document.querySelector('.about-country'),
+};
+
+export default refs;
